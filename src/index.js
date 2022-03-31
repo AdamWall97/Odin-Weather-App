ea39f14f5e5f0fe0de4f438c5ed20e84132ee6bb
AdamWall97 <51244839+AdamWall97@users.noreply.github.com>
2022-03-31 16:47:14 -0500

    Update index.js
    Remove API Key Oops

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 function getWeatherData()  {
-    
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.9778&lon=93.2650&appid=b465203765cd4985338bab877bf8fbb5',{mode: 'cors'})
+    {mode: 'cors'})
         .then(function(response){
             return response.json();
         })
